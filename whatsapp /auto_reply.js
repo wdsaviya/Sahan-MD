f3dcefc836e164f610c4eb146df5b4a822b786ee
Sahan Maduwantha <125999503+maduwa2006@users.noreply.github.com>
2024-04-14 13:51:54 -0400

    Create auto_reply.js

---
+++ whatsapp /auto_reply.js
@@ -0,0 +1,3 @@
+{
+  "dark shan": "𝐻𝐸𝐿𝐿𝑂 𝐼𝑀 𝐷𝐴𝑅𝐾 𝑆𝐻𝐴𝑁 𝑌𝑇 \n\n𖣔ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ𖣔\nhttps://youtube.com/@darkshanyt1\n\n𖤍ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ɢʀᴏᴜᴘ𖤍\nhttps://chat.whatsapp.com/BxXB834YgV8JorX5tCDEEM\n\n☃︎ᴛʜᴀɴᴋs ғᴏʀ ʏᴏᴜ ᴍᴀssᴇɢᴇ ☃︎"
+}
