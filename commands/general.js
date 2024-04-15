15764029471caee964b2ec36573ac181ed3f2354
Sahan Maduwantha <125999503+maduwa2006@users.noreply.github.com>
2024-04-14 22:55:27 -0400

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -1,73 +1,192 @@
 /**
-//════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                                    //
-//                                             W H A T S A P P _ U S E R _ BOT                                        //
-//                                                                                                                    //
-//                                                     V E R S I O N：１.0                                             //
-//                                                                                                                    //
-//            ███╗░░░███╗░░█████╗░░███████╗░████████░░███████╗░████████╗░░░░░░░░░░░░███╗░░░███╗░░██████╗░░░░░         //   
-//            ████╗░████║░██╔══██╗░██╔════╝░╚══██║══╝░██║════╝░██╔═══██╗░░░░░░░░░░░░████╗ ████║░░██╔══██╗░░░░         //
-//            ██╔████╔██║░███████║░███████╗░░░░██║░░░░███████║░███████╔╝░░███████░░░██╔████╔██║░░██║░░██║░░░░         // 
-//            ██║░██║╚██║░██╔══██║░╚════██║░░░░██║░░░░██║════╝░██╔═══██╗░░╚══════╝░░██║░██░░██║░░██║░░██║░░░░         //
-//            ██║░╚═╝░██║░██║░░██║░███████║░░░░██║░░░░███████║░██║░░░░██░░░░░░░░░░░░██║░╚═╝░██║░░██████╔╝░░░░         //
-//            ╚═╝░░░░░╚═╝░╚═╝░░╚═╝░╚══════╝░░░░╚═╝░░░░░╚══════╝░╚═╝░░░░╚═╝░░░░░░░░░░░╚═╝░░░░░╚═╝░░╚═════╝░░░░░        //
-//                                                                                                                    //
-//                                 C R E A T E D _ B Y _ M R _ S A H A N _ O F C _ S L _ R G                          //  
-//                                                                                                                    //
-//                                                                                                                    //
-//════════════════════════════════════════════════════════════════════════════════════════════════════════════════════//
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : BLADE-MD
+ * @author : salmanytofficial <https://github.com/Bladeh4x>
+ * @description : BLADE, A Multi-functional whatsapp bot.
+ * @version 0.0.6
  **/
-const { formatp , formatDate , tlang, botpic,cmd, prefix, runtime,Config , parsedJid ,sleep } = require('../lib')
+
+const { tlang, botpic, cmd, prefix, runtime, Config , sleep } = require('../lib')
 const axios = require('axios')
-const fetch = require('node-fetch');
- const os = require('os')
 const speed = require('performance-now')
-const API_KEY = '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'
-//===============================================
-      async function getDateTime() {
-        const now = new Date();
-        const date = now.toISOString().slice(0, 10);
-        const time = now.toLocaleTimeString();
-        return { date, time };
-      }
-///=============================================
+const fetch = require('node-fetch');
+
+
+const PastebinAPI = require("pastebin-js");
+pastebin = new PastebinAPI("EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL");
 cmd({
-  pattern: "uptime",
-  alias: ["runtime"],
-  desc: "Tells runtime/uptime of bot.",
-  category: "misc",
-  filename: __filename
+        pattern: "pastebin",
+        desc: "To check ping",
+        category: "extra",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        if(!citel.quoted) return citel.reply('Please quote any text to get link.')
+        let data = await pastebin.createPaste(citel.quoted.text, "Secktor-Pastebin")
+        citel.reply('_Here is your link._\n'+data)
+    }
+);
+
+
+//---------------------------------------------------------------------------
+cmd({
+    pattern: "chat",
+    alias :['gpt'],
+    desc: "chat with an AI(GPT)",
+    category: "AI",
+    use: '<◡̈⋆🅷🅸(●’◡’●)ﾉ,𝚂𝚝𝚊𝚛>',
+    filename: __filename,
 },
-async(darkshan, msg, text) => {
-  const upt = runtime(process.uptime())
-  msg.reply(`*_Uptime of master md: ${upt}_*`)
+async(Void, citel,text) => {
+    let zx = text.length;
+    if (zx < 8) {
+        let {data} = await axios.get(`http://api.brainshop.ai/get?bid=167991&key=aozpOoNOy3dfLgmB&uid=[${citel.sender.split("@")[0]}]&msg=[${text}]`);
+        return citel.reply(data.cnt);  
+    }
+    if (!text) return citel.reply(`Hey there! ${citel.pushName}. How are you doing these days?`);
+    // const { Configuration, OpenAIApi } = require("openai");
+    // const configuration = new Configuration({
+    //     apiKey: Config.OPENAI_API_KEY || "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",
+    // });
+    // const openai = new OpenAIApi(configuration);
+    // const completion = await openai.createCompletion({
+    //     model: "text-davinci-002",
+    //     prompt: text,
+    //     temperature: 0.5,
+    //     max_tokens: 80,
+    //     top_p: 1.0,
+    //     frequency_penalty: 0.5,
+    //     presence_penalty: 0.0,
+    //     stop: ['"""'],
+    // });
+    // citel.reply(completion.data.choices[0].text);
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${Config.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo", 
+      messages: [{ role: "system", content: "You" }, { role: "user", content: text }],
+    }),
+  });
+
+  const data = await response.json();
+  console.log("GPT REPONCE : ",data); 
+  if (!data.choices || data.choices.length === 0) {citel.reply("*Invalid ChatGPT API Key, Please Put New Key*"); }
+  return await  citel.reply(data.choices[0].message.content)
+	
 }
 )
-//---------------------------------------------------------------------------
-//                  RREPOSITORY  COMMAND
+
+cmd({
+    pattern: "dalle",
+    alias : ['dall','dall-e'],
+    desc: "Create Image by AI",
+    category: "AI",
+    use: '<an astronaut in mud.>',
+    filename: __filename,
+},
+async(Void, citel,text,{isCreator}) => 
+{
+//if (!isCreator) return citel.reply(tlang().owner)
+if (Config.OPENAI_API_KEY=='') return citel.reply('You Dont Have OPENAI_API_KEY \nPlease Create OPEN API KEY from Given Link \nhttps://platform.openai.com/account/api-keys');
+if (!text) return citel.reply(`*Give Me A Query To Get Dall-E Reponce ?*`); 
+const imageSize = '256x256'
+const apiUrl = 'https://api.openai.com/v1/images/generations';
+const response = await fetch(apiUrl, {
+method: 'POST',
+headers: {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${Config.OPENAI_API_KEY}`
+},
+body: JSON.stringify({
+  model: 'image-alpha-001',
+  prompt: text,
+  size: imageSize ,
+  response_format: 'url'
+})
+});
+
+const data = await response.json();
+let buttonMessage = {
+    image:{url:data.data[0].url},
+    caption : '*---Your DALL-E Result---*'
+
+}
+
+Void.sendMessage(citel.chat,{image:{url:data.data[0].url}})
+}
+)
+
 //---------------------------------------------------------------------------
 cmd({
+        pattern: "alive2",
+        alias: ["about","starz"],
+        desc: "To check bot alive state",
+        category: "MASTER",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        const uptime = process.uptime();
+        timestampe = speed();
+        latensie = speed() - timestampe;
+        let ter = `
+ㅤ ❁    🔰MASTER-ALIVE-2🔰     
+╰─➤｡･:*˚:✧｡ *${tlang().title}* ｡･:*˚:✧｡
+╰─➤*🌟Description:* A WhatsApp bot with rich features, built in NodeJs to make your WhatsApp enjoyable.
+╰─➤*⚡️Speed:* ${latensie.toFixed(4)} ms
+╰─➤*⏱Uptime:* ${runtime(process.uptime())}
+╰─➤*🌟Version:* 10.0.0
+╰─➤*👤Owner:*  ${Config.ownername}
+╰─➤*Powered by ${tlang().title}*
+°୭̥ ❁ ───────────────────────── .°୭̥ ❁ `;
+        let buttonMessaged = {
+            image: {
+                url: await botpic(),
+            },
+            caption: ter,
+            footer: tlang().footer,
+            headerType: 4,
+            contextInfo: {
+                externalAdReply: {
+                    title: tlang().title,
+                    body: `Bot-Status`,
+                    thumbnail: log0,
+                    mediaType: 2,
+                    mediaUrl: ``,
+                    sourceUrl: ``,
+                },
+            },
+        };
+        return await Void.sendMessage(citel.chat, buttonMessaged, {
+            quoted: citel,
+        });
+
+    }
+)
+
+//---------------------------------------------------------------------------
+
+cmd({
         pattern: "repo",
-        alias: ["git", "sc", "script"],
+        alias: ["star", "sc","script"],
         desc: "Sends info about repo.",
         category: "general",
         filename: __filename,
     },
     async(Void, citel) => {
         let { data } = await axios.get('https://api.github.com/repos/maduwa2006/MASTER-MD')
-        let cap = `💈 *MASTER MD REPO* \n
-*⭐ Total Stars:* ${data.stargazers_count} stars
-
-*🍽️ Forks:* ${data.forks_count} forks
-
-*🍁 Repo:* https://github.com/maduwa2006/MASTER-MD
-🎏️️ *Group:*
- https://whatsapp.com/channel/0029VaFLAgi90x2oD70Hwq1z
-
-🀄 *developer:*-Sahan Maduwantha
-
-
-©️ *MASTER MD 2024*`
+        let cap = `Hey ${citel.pushName}\n
+*⭐ TOTAL STARS:* ${data.stargazers_count} stars
+*🍽️ FORKS:* ${data.forks_count} forks
+*🍁 REPO:*https://github.com/maduwa2006/MASTER-MD
+*⚔️SUPPORT:* wa.me/+94720797915
+*💻ᑕᕼᗩᑎᑎᗴᒪ ᒪIᑎK:* https://whatsapp.com/channel/0029VaWWZa1G3R3c4TPADo0M
         let buttonMessaged = {
             image: { url: await botpic() },
             caption: cap,
@@ -75,7 +194,7 @@ cmd({
             headerType: 4,
             contextInfo: {
                 externalAdReply: {
-                    title: "MASTER MD-REPO",
+                    title: "STAR-REPO",
                     body: "Easy to Use",
                     thumbnail: log0,
                     mediaType: 4,
@@ -91,8 +210,6 @@ cmd({
     }
 )
 //---------------------------------------------------------------------------
-//                  BOT STATUS COMMAND
-//---------------------------------------------------------------------------
 cmd({
         pattern: "status",
         alias: ["about"],
@@ -105,13 +222,14 @@ cmd({
         timestampe = speed();
         latensie = speed() - timestampe;
         let ter = `
-💐 *MASTER MD* 💐
-*🌟Description:* A WhatsApp bot with rich features, build in NodeJs to make your WhatsApp enjoyable.
+🔰 *${tlang().title}* 🔰
+𝐎𝐖𝐍𝐄𝐑:- +94720797915
+*🌟Description:* 𝙰 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 𝚋𝚘𝚝 𝚠𝚒𝚝𝚑 𝚛𝚒𝚌𝚑 𝚏𝚎𝚊𝚝𝚞𝚛𝚎𝚜, 𝚋𝚞𝚒𝚕𝚝 𝚒𝚗 𝙽𝚘𝚍𝚎𝙹𝚜 𝚝𝚘 𝚖𝚊𝚔𝚎 𝚢𝚘𝚞𝚛 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 𝚎𝚗𝚓𝚘𝚢𝚊𝚋𝚕𝚎...𝚋𝚢 Master.
 *⚡Speed:* ${latensie.toFixed(4)} ms
 *🚦Uptime:* ${runtime(process.uptime())}
-*🕸Version:* 0.0.1
+*🕸Version:* 0.0.7
 *👤Owner:*  ${Config.ownername}
-🎭 *POWER BY MASTER MD*
+*Powered by ${tlang().title}*
 `;
         let buttonMessaged = {
             image: {
@@ -122,8 +240,8 @@ cmd({
             headerType: 4,
             contextInfo: {
                 externalAdReply: {
-                    title: `kushansewmina`,
-                    body: `Dark shan-Status`,
+                    title: tlang().title,
+                    body: `Bot-Status`,
                     thumbnail: log0,
                     mediaType: 2,
                     mediaUrl: ``,
@@ -138,63 +256,7 @@ cmd({
     }
 )
 
-//========================================================================
-
-cmd({
-  pattern: "cpu",
-  desc: "To check bot status",
-  category: "general",
-  filename: __filename,
-},
-async(darkshan, msg) => {
-  try{
-      const used = process.memoryUsage()
-      const cpus = os.cpus().map(cpu => {
-          cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-          return cpu
-      })
-      const cpu = cpus.reduce((last, cpu, _, { length }) => 
-      {
-
-          last.total += cpu.total
-          last.speed += cpu.speed / length
-          last.times.user += cpu.times.user
-          last.times.nice += cpu.times.nice
-          last.times.sys += cpu.times.sys
-          last.times.idle += cpu.times.idle
-          last.times.irq += cpu.times.irq
-          return last
-      },{ speed: 0,total: 0,times: {user: 0,nice: 0,sys: 0,idle: 0,irq: 0 } }
-      )
-    timestampe = speed();
-    latensie = speed() - timestampe;
-    var neww = performance.now()
-    var oldd = performance.now()
-                  
-    respon = `*❲❒❳ dark shan md Server Info ❲❒❳*
-    
-  *❲❒❳ Runtime:* ${runtime(process.uptime())}
-  *❲❒❳ Speed:* ${latensie.toFixed(3)}/${(oldd - neww).toFixed(3)} ms
-  *❲❒❳ RAM:* ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
-  
-  *❲❒❳ Memory Usage:*
-      ${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n      ')}
-  
-${cpus[0] ? `  *❲❒❳ Total CPU Usage:*
-  *${cpus[0].model.trim()} (${cpu.speed} MHZ)*
-      ${Object.keys(cpu.times).map(type => `-${(type + '').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n      ')}
-\n  *❲❒❳ CPU Core Usage (${cpus.length} Core CPU)*
-  ${cpus.map((cpu, i) => `*Core ${i + 1}: ${cpu.model.trim()} (${cpu.speed} MHZ)*
-      ${Object.keys(cpu.times).map(type => `-${(type + '').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n      ')}`).join('\n\n')}` : ''}
-`.trim()
-      return await msg.send(respon,{},"",msg)
-  }catch(e){msg.error(e)}
-})
- 
-
-
-  //=========================================================
-
+//---------------------------------------------------------------------------
 cmd({
     pattern: "theme",
     desc: "To find all themes",
@@ -204,10 +266,9 @@ cmd({
 async(Void, citel,text,{isCreator}) => {
 
 if(!isCreator) return citel.reply(tlang().owner);
-let str="*All available themes in MASTER*"
-str+=`
-1. MASTER\n2.SECKTOR these are the themes of Secktor Userbot.\_Reply ${prefix}setvar THEME:MASTER`
+let str="*All available themes*"
+str+=`  \n1. MASTER\n2. SONIC-MD\n3. AYANOKOJI\n4. DEKU\n5. RENGOKU\n6. GENOS\n7. GABIMARU\n8. STAR\n9. ERENJAEGER\n10. LUFFY\n11. NARUTO\n12. NEZUKO\n13. PARKER\n14. GOKU\n15. MAKIMA\n16. THOMAS\n17. PATRICK\n\n these are the themes of Star Userbot.\_Reply ${prefix}setvar THEME:ZEROTWO`
 return citel.reply(str)
-    
+
 }
 )
